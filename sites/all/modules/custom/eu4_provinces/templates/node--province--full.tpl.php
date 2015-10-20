f4c6f093d7d0cb4974b6b24399eb6b2d337cbae3
Nicoloye <nicoloye@gmail.com>
2015-10-20 21:47:01 +0200

    Added provinces, work in progress.

---
+++ sites/all/modules/custom/eu4_provinces/templates/node--province--full.tpl.php
@@ -0,0 +1,6 @@
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+  <?php print $content_eu4_provinces_history; ?>
+</div>
+<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+  <?php print $content_eu4_provinces_common_names; ?>
+</div>
