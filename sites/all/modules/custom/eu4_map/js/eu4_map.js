e78e8a42d5ab244f3f74da1b73cd5491a6e6011b
Nicoloye <nicoloye@gmail.com>
2015-10-25 21:36:04 +0100

    Added province association behavior on the map.

--- sites/all/modules/custom/eu4_map/js/eu4_map.js
+++ sites/all/modules/custom/eu4_map/js/eu4_map.js
@@ -6,47 +6,54 @@
 (function ($) {
   Drupal.behaviors.eu4_map = {
     attach: function (context, settings) {
-      /*
-      $('#eu4_map', context)
-      .zoomPanTouchSVG();*/
-      var initial_viewBox = $('#eu4_map', context)[0].viewBox.baseVal;
       
-      $('#eu4_map', context).svgPanZoom({
-        animationTime: 0,
-        zoomFactor: 0.25,
-        maxZoom: 3,
-        panFactor: 100,
-        initialViewBox: {
-          x: initial_viewBox.x,
-          y: initial_viewBox.y,
-          width: initial_viewBox.width,
-          height: initial_viewBox.height
-        },
-        limits: {
-            x: initial_viewBox.x,
-            y: initial_viewBox.y,
-            x2: initial_viewBox.width,
-            y2: initial_viewBox.height
-        }
-      });
-      
-      /*$('map svg', context).live('mousedown', function(e) {
-        if (e.which == 2) {
-          // Store the initial value if not already stored.
-          if ($(this).data('viewBox') == null) {
-            $(this).data('viewBox', $(this).attr('viewBox'));
+      if ($('#eu4_map', context).length) {
+        var last_selected = $('#eu4_map g', context);
+        
+        $('#eu4_map g', context).click(function(){
+          // If no toolbox is displayed, build one.
+          $('#province_selector').remove();
+          if ($(this).attr('selected') == 'selected') {
+            $(this).parents('map').append('<div id="province_selector"></div>');
+            var position = $(this).offset();
+            $('#province_selector').css({
+              position: 'absolute',
+              left: position.left,
+              top: position.top
+            });
+            
+            // Load the drupal form.
+            var id = $(this).attr('id');
+            $('#province_selector').load(Drupal.settings.basePath + 'eu4-map/associate/province/' + id);
           }
           
-          if ($(this).data('viewBox') == $(this).attr('viewBox')) {
-            console.log('debug', $(this).data('viewBox'));
-            $(this).setAttribute('viewBox', '0 0 ' + 200 + ' ' + 200);
-          }
-        }
-        e.preventDefault();
-      }).live('contextmenu', function(e){
-        console.log('debug', e);
-        //e.preventDefault();
-      });*/
+          // Select the current path.
+          last_selected.removeAttr('selected');
+          $(this).attr('selected', 'selected');
+          last_selected = $(this);
+        });
+        
+        // Zoom and pan.
+        var initial_viewBox = $('#eu4_map', context)[0].viewBox.baseVal;
+        $('#eu4_map', context).svgPanZoom({
+            animationTime: 0,
+            zoomFactor: 0.25,
+            maxZoom: 3,
+            panFactor: 100,
+            initialViewBox: {
+              x: initial_viewBox.x,
+              y: initial_viewBox.y,
+              width: initial_viewBox.width,
+              height: initial_viewBox.height
+            },
+            limits: {
+                x: initial_viewBox.x,
+                y: initial_viewBox.y,
+                x2: initial_viewBox.width,
+                y2: initial_viewBox.height
+            }
+        });
+      }
     }
   };
 })(jQuery);
---
+++ sites/all/modules/custom/eu4_map/templates/page--eu4-map--associate--province.tpl.php
@@ -0,0 +1,5 @@
+<div id="content" class="column">
+  <div class="section">
+    <?php print render($page['content']); ?>
+  </div>
+</div>
