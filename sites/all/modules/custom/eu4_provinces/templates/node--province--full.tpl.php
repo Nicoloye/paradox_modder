4771fcd1a5e5d0863aeb126e9a3bc1c200252bd2
Nicoloye <nicoloye@gmail.com>
2015-11-22 00:34:49 +0100

    Added climate and trade winds on province previews.

--- sites/all/modules/custom/eu4_provinces/templates/node--province--full.tpl.php
+++ sites/all/modules/custom/eu4_provinces/templates/node--province--full.tpl.php
@@ -1,6 +1,18 @@
 <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
   <?php print $content_eu4_provinces_history; ?>
 </div>
-<div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
-  <?php print $content_eu4_provinces_common_names; ?>
-</div>
+<?php if (isset($content_eu4_provinces_map_climate)): ?>
+  <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+    <?php print $content_eu4_provinces_map_climate; ?>
+  </div>
+<?php endif; ?>
+<?php if (isset($content_eu4_provinces_map_trade_winds)): ?>
+  <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+    <?php print $content_eu4_provinces_map_trade_winds; ?>
+  </div>
+<?php endif; ?>
+<?php if (isset($content_eu4_provinces_common_names)): ?>
+  <div id="node-<?php print $node->nid; ?>" class="<?php print $classes; ?>"<?php print $attributes; ?>>
+    <?php print $content_eu4_provinces_common_names; ?>
+  </div>
+<?php endif; ?>
